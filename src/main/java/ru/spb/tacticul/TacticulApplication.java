82ae4d7cea6ff63ccfdf337ae3bda02d5d5e4ffa
Y11o <Y11o@users.noreply.github.com>
2025-02-06 14:54:04 +0300

    Initial

---
+++ src/main/java/ru/spb/tacticul/TacticulApplication.java
@@ -0,0 +1,13 @@
+package ru.spb.tacticul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacticulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacticulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/spb/tacticul/TacticulApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spb.tacticul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacticulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
